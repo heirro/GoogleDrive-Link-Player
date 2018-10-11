366fe6bbdd44fd77abc6feeff6faa50c0d6b75e1
Vava Heirro <vavaheirro@live.com>
2018-10-11 10:48:14 +0700

    adding title copyright, logs v1.1

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-<hr><center><p style="font-size:12px;"><a onclick="href='https://github.com/heirro'" role="button" target="_blank">Fork GitHub Developer</a></p></center></body></html>
+<hr><center><p style="font-size:12px;"><a onclick="href='https://github.com/heirro'" role="button" target="_blank">Fork on GitHub Developer</a> | &copy; Copyright - 2018</p></center></body></html>
