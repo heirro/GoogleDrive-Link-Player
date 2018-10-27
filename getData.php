132e6fbeadbfd00e99d0263d8f6560150197445e
Vava Heirro <vavaheirro@live.com>
2018-10-27 16:00:06 +0700

    update variable & method

--- getData.php
+++ getData.php
@@ -1,11 +1,14 @@
 <?php
-$fGD = $_POST['files'];
- $sumber = "https://api.heirro.net/developers/gdrive/get.json?u=".$fGD;
- $konten = file_get_contents($sumber);
- $data = json_decode($konten, true);
-
+$method = $_POST['files']; // Method GET data via POST Form //
+/////////////////////
+ $search = "https://api.heirro.net/developers/gdrive/get.json?u=".$method;
+ $result = file_get_contents($search);
+ $data = json_decode($result, true);
+/////////////////////
    $linkStream = $data['streaming_jwplayer'];
+   $linkStream02 = $data['streaming_html5'];
    $linkDown = $data['download_key'];
    $getFileName = $data['filename'];
    $getImage = $data['preview'];
+   $linkDownPrivate = $data['download_host'];
 ?>
